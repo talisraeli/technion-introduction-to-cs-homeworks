438b3d3261df0d88ee3f0bc0a24dd13990f43beb
KeshetBehanan <contact@talisrae.li>
2025-10-24 00:27:55 +0300

    Homework 0

---
+++ hw0/hw0q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Counting from 3
+    for(int i = 3; i > 0; i--) {
+        printf("%d\n", i);
+    }
+    printf("Hello, World!\n");
+    /*
+     * Well,
+     * Are you ready?
+    */
+    return 0;
+}
